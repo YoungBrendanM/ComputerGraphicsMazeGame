827a342a1e4f85d42ae1a547097a514d339ebc18
JamesDonovan1 <jmdm4r@mail.missouri.edu>
2022-04-26 20:15:05 -0500

    Class Renaming
    C# Classes cannot start with a number, renamed initial 2d maze generating script

--- Assets/Maze_Generation_2D.cs
+++ Assets/Maze_Generation_2D.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 2d_maze_generation : MonoBehaviour
+public class Maze_Generation_2D : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class 2d_maze_generation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ;
     }
 }
